780f8ff7177012c8836eed37c2de5efe045ed8e0
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-09-24 01:54:26 +0300

    login user and logout user

--- app/auth/views.py
+++ app/auth/views.py
@@ -30,4 +30,28 @@ def register():
             db.session.commit()
             flash('Account created successfully. Please Login')
             return redirect(url_for('auth.login'))
-    return render_template('auth/register.html', form=form)
+    title = "Create New Account"
+    return render_template('auth/register.html', form=form, title=title)
+
+
+# login route
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password')
+    title = "Login to your account"
+    return render_template('auth/login.html', form=form, title=title)
+
+
+# logout route
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for('main.index'))
