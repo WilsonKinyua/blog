459ad775bad7b97cb947abee928d47ffe2b2eb5a
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-09-24 01:38:41 +0300

    login and form class defined

---
+++ app/auth/forms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import Required, Email, Length, EqualTo
+from ..models import User
+from wtforms import ValidationError
+
+
+class LoginForm(FlaskForm):
+    """
+        Login form
+    """
+    email = StringField('Email', validators=[Required(), Email()])
+    password = PasswordField('Password', validators=[Required()])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    """
+        Registration form
+    """
+    email = StringField('Email', validators=[
+                        Required(), Email(), Length(1, 64)])
+    username = StringField('Username', validators=[Required(), Length(1, 64)])
+    password = PasswordField('Password', validators=[Required(), EqualTo(
+        'password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[Required()])
+    submit = SubmitField('Register')
+
+    def validate_check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered. Please proceed to login!')
+
+    def validate_check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use. Please use another username.')
