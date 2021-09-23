a4dc0df78622ba7558d443018df6abc38779006a
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-09-24 02:36:32 +0300

    render homepage view done

---
+++ app/main/views.py
@@ -0,0 +1,21 @@
+from flask import render_template, redirect, url_for, abort, flash, request
+from . import main
+from flask_login import login_required, current_user
+from ..models import User, Role, Post, Comment, Category
+from .. import db, photos
+# from .forms import UpdateProfileForm, CommentForm, CategoryForm
+from slugify import slugify
+
+
+# homepage
+@main.route('/')
+def index():
+    """
+        View root page function that returns the index page and its data
+    """
+    # get all posts
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    # get all categories
+    categories = Category.query.all()
+
+    return render_template('index.html', posts=posts, categories=categories)
